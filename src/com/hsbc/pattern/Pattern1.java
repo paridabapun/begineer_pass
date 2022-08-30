739300aa367bf2424fe2534762a2ee44f25b56df
Bhagabata Parida <paridabapun@gmail.com>
2022-08-30 15:23:59 +0530

    Pattern1

--- src/com/hsbc/pattern/Pattern1.java
+++ src/com/hsbc/pattern/Pattern1.java
@@ -1,11 +1,31 @@
 package com.hsbc.pattern;
 
+import java.util.Scanner;
+
+
+/* 
+
+****
+****
+****
+****
+ 
+ */
 public class Pattern1 {
-	
+
 	public static void main(String[] args) {
+		Scanner  scanner = new Scanner(System.in);
 		
+		int n = scanner.nextInt();
+		for(int row = 1; row <= n; row ++) {
+			
+			for(int col = 1; col <= n ;  col ++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
-		System.out.println("Bapun Parida");
 	}
 
 }
--- src/com/hsbc/pattern/Pattern2.java
+++ src/com/hsbc/pattern/Pattern2.java
@@ -1,5 +1,10 @@
 package com.hsbc.pattern;
 
 public class Pattern2 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Bapun");
+	}
 
 }
