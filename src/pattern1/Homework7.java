c081c2132a140e71f9f3efe82be327d1266bfc9b
Bhagabata Parida <paridabapun@gmail.com>
2023-01-26 10:37:21 +0530

    Modified

--- src/pattern1/Homework7.java
+++ src/pattern1/Homework7.java
@@ -1,5 +1,75 @@
 package pattern1;
 
+import java.util.Scanner;
+
 public class Homework7 {
 
+	/*
+	 * 
+	 * SPhoto Frame Pattern
+
+Problem Description
+Take an integer N as input, print the corresponding pattern for N.
+
+For example if N = 5 then pattern will be like:
+
+*****
+*   *
+*   *
+*   *
+*****
+Note : Except for the first line and the last line, every line will have N-2 spaces between the two stars.
+
+
+Problem Constraints
+3 <= N <= 100
+
+
+Input Format
+First and only line of input contains a single integer N.
+
+
+
+Output Format
+Output the pattern corresponding to the given N.
+
+
+
+Example Input
+Input 1:
+
+ 3
+Input 2:
+
+ 4
+
+
+Example Output
+Output 1:
+
+***
+* *
+***
+Output 2:
+
+ 
+****
+*  *
+*  *
+****
+
+
+Example Explanation
+ Print the pattern as described.
+
+
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+
+	}
 }
