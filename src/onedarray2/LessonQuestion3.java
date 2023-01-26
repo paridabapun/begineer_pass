720866d2b50f41183a1125867689b8d5eecb245c
Bhagabata Parida <paridabapun@gmail.com>
2023-01-27 01:13:36 +0530

    Added

--- src/onedarray2/LessonQuestion3.java
+++ src/onedarray2/LessonQuestion3.java
@@ -9,10 +9,9 @@ adjacent pairs if the differ equal k return true else return false.
 public class LessonQuestion3 {
 	static boolean checkDiff(int arr[], int k) {
 
-		
-		//Tricky one 
+		// Tricky one
 		for (int i = 0; i <= arr.length - 2; i++) {
-			if(arr[i+1] -arr[i] == k) {
+			if (arr[i + 1] - arr[i] == k) {
 				return true;
 			}
 		}
@@ -21,7 +20,7 @@ public class LessonQuestion3 {
 	}
 
 	public static void main(String[] args) {
-		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0,3 };
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 3 };
 		boolean searchResult = checkDiff(arr, 15);
 		System.out.println(searchResult);
 	}
